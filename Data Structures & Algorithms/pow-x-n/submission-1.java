7f49d95d6b587110f2ffabec48089792f289227b
Ayush Singla <101383376+Ayush1665@users.noreply.github.com>
2026-04-16 14:32:18 +0530

    Add: pow-x-n - submission-1

---
+++ Data Structures & Algorithms/pow-x-n/submission-1.java
@@ -0,0 +1,11 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n == 0) return 1;
+        if(x == 0) return 0;
+        double res = 1;
+        for (int i = 0; i < Math.abs(n); i++) {
+            res = res * x;
+        }
+        return n >= 0 ? res : 1 / res;
+    }
+}
